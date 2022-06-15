55f54ecceee6be0bbb0ccad31ff9c4500a0faa4e
RudhiKurniawan <20014152@myrp.edu.sg>
2022-06-16 07:25:19 +0800

    fixed typo in userresigstercustomer fixed typo in delete company popup added requirements for company registration added requirements for delivery change operations to font awesome logos

--- Models/Company.cs
+++ Models/Company.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace WebApplication1.Models
     public class Company
     {
         public int CompanyId { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Company Name")]
         public string CompanyName { get; set; }
 
         
--- Models/Delivery.cs
+++ Models/Delivery.cs
@@ -11,31 +11,43 @@ namespace WebApplication1.Models
 
       public int DeliveryId { get; set; }
 
+        [Required(ErrorMessage = "Please enter Customer's Full Name")]
         public string FullName { get; set; }
 
+        
         public int CompanyId{ get; set; }
 
         public string CompanyName { get; set; }
 
+
+        [Required(ErrorMessage = "Please enter Details of Delivery")]
         public string Details { get; set; }
 
       public string CountryFrom { get; set; }
 
       public string CountryTo { get; set; }
 
-      public double Distance { get; set; }
 
-      public double WeightPackage { get; set; }
+        [Required(ErrorMessage = "Please enter Delivery Distance")]
+        public double Distance { get; set; }
 
-      public double VehicleWeight { get; set; }
+        [Required(ErrorMessage = "Please enter Package Weight")]
+        public double WeightPackage { get; set; }
 
-      public double TotalWeight { get; set; }
-      public double VehicleSpeed { get; set; }
+        [Required(ErrorMessage = "Please enter Vehicle Weight")]
+        public double VehicleWeight { get; set; }
 
-      public int VehicleId { get; set; }
+        [Required(ErrorMessage = "Please enter Total Weight of Vehicle")]
+        public double TotalWeight { get; set; }
 
-      public string VehicleType { get; set; }
+        [Required(ErrorMessage = "Please enter Speed of Vehicle")]
+        public double VehicleSpeed { get; set; }
 
-      public double CarbonEmi { get; set; }
+        public int VehicleId { get; set; }
+
+        public string VehicleType { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Carbon Emission")]
+        public double CarbonEmi { get; set; }
     }
 }
